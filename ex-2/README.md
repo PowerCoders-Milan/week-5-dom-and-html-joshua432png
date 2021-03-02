44d1be25268a13db5e72ff56f1fe6292b0c9b9de
Joshua Udeh <udehasilo@gmail.com>
2021-03-02 14:17:09 +0100

    exercise 2

--- ex-2/README.md
+++ ex-2/README.md
@@ -3,5 +3,5 @@
 Write a JavaScript function to get the value of the href, hreflang, rel, target, and type attributes of the specified link.
 
 
-Print all the valueas like this:
+Print all the values like this:
 - 'The value of the href attribute of the link is : -value-'
--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,11 @@
 function getAttributes() {
-
+    
+    var body = document.getElementById("pwcdr").getAttribute("type")
+    alert(" The value of the href attribute of the link is " + body)
+    var href =  document.getElementById("pwcdr").getAttribute("hreflang")
+    alert(" The value of the href attribute of the link is " + href)
+    var rel = document.getElementById("pwcdr").getAttribute("rel")
+    alert(" The value of the href attribute of the link is " + rel)
+    var target = document.getElementById("pwcdr").getAttribute("target")
+    alert(" The value of the href attribute of the link is " + target)
 }
