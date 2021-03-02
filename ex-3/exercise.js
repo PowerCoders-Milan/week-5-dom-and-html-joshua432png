1b88e28d323cd438c1d0a84c57342573812e63e0
Joshua Udeh <udehasilo@gmail.com>
2021-03-02 14:48:16 +0100

    exercise 3

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -3,6 +3,6 @@ function insert_Row() {
     var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
-    cell1.innerHTML = "Row1 cell1";
-    cell2.innerHTML = "Row1 cell2";
+    cell1.innerHTML = "NEW CELL1";
+    cell2.innerHTML = "NEW CELL2";
 }
