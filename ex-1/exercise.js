6a72aaa310c64f5310c49a01a1640798c89f0195
Joshua Udeh <udehasilo@gmail.com>
2021-03-02 11:49:02 +0100

    exercise 1

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,5 +1,7 @@
 // Use this function that is linked to the button to change the background
 
 function set_background() {
-
+    document.getElementsByTagName("p")[0].style.backgroundColor = "red";
+    document.getElementsByTagName("p")[1].style.backgroundColor = "orange";
+    document.getElementsByTagName("p")[2].style.backgroundColor = "blue";
 }
